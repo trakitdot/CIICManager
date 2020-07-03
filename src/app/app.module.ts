05ddcff76ebfa01b59a52f0fd40f6d9db83924e6
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2020-07-03 13:04:06 +0200

    Revert "there is an error"
    This reverts commit 8d219bf32809b4af075fbb9fb0f72c304aff1cfe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClient, HttpHeaders, HttpParams, HttpHandler } from '@angular/common/http';
-import { HTTP } from '@ionic-native/http/ngx';
 
-//import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule/*,IonicStorageModule.forRoot({
-    name: '__mydb',
-driverOrder: ['indexeddb', 'sqlite', 'websql', 'localstorage']
-  })*/],
-  providers: [HTTP,HttpClient, HttpHandler,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
